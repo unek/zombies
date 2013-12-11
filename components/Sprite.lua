f7c5523fdcbb70580c1814c62090a0ee0802de7b
unekpl <unekpl@gmail.com>
2013-12-11 19:30:45 +0100

    sprite now uses only love.graphics.draw

--- components/Sprite.lua
+++ components/Sprite.lua
@@ -10,14 +10,7 @@ function Sprite:draw()
     local sx = self.width / self.image:getWidth()
     local sy = self.height / self.image:getHeight()
     love.graphics.setColor(255, 255, 255, 255)
-
-    love.graphics.push()
-        love.graphics.translate(self.pos.x, self.pos.y)
-        love.graphics.rotate(self.rotation)
-        love.graphics.translate(-self.pos.x, -self.pos.y)
-        love.graphics.translate(-self.width/2, -self.height/2)
-        love.graphics.draw(self.image, self.pos.x, self.pos.y, 0, sx, sy)
-    love.graphics.pop()
+    love.graphics.draw(self.image, self.pos.x, self.pos.y, self.rotation, sx, sy, (self.width/2)/sx, (self.height/2)/sy)
 end
 
 function Sprite:setWidth(w)
