a9fdaaf453ab6f7a5ec6cfa259784e88e0ab350e
Nixola <niki.or97@yahoo.it>
2013-12-12 19:39:27 +0100

    Fixed indentation

--- Config.lua
+++ Config.lua
@@ -4,55 +4,55 @@ Config.default = {debug=true, lighting=true}
 
 function Config:read(t)
 
-  local ConfigFile = love.filesystem.newFile("config", "r")
+    local ConfigFile = love.filesystem.newFile("config", "r")
 
-  self.indexes = {}
-  local n = 0
+    self.indexes = {}
+    local n = 0
 
-  if not ConfigFile then
-    
-    for index, value in pairs(self.default) do
-      n = n + 1
+    if not ConfigFile then
+        
+        for index, value in pairs(self.default) do
+            n = n + 1
 
-      t[index] = value
-      self.indexes[n] = index
+            t[index] = value
+            self.indexes[n] = index
 
+        end
+
+    else
+
+        for line in ConfigFile:lines() do
+            n = n + 1
+
+            local index, value = line:match("([%l_][%l%d_]*)%:(.+)")
+            t[index] = loadstring("return "..value)()
+            self.indexes[n] = index
+
+        end
+
+        ConfigFile:close()
     end
-
-  else
-
-    for line in ConfigFile:lines() do
-      n = n + 1
-
-      local index, value = line:match("([%l_][%l%d_]*)%:(.+)")
-      t[index] = loadstring("return "..value)()
-      self.indexes[n] = index
-
-    end
-
-    ConfigFile:close()
-  end
 
 end
 
 
-function Config:save(t)
-  
-  local ConfigFile = love.filesystem.newFile("config", "w")
+function Config:save(t, indexes)
+    
+    local ConfigFile = love.filesystem.newFile("config", "w")
 
-  for _, index in ipairs(self.indexes) do
-    local value = t[index]
+    for _, index in ipairs(indexes or self.indexes) do
+        local value = t[index]
 
-    value = type(value) == "function" and string.dump(value) or value
-    ConfigFile:write(index .. ":" .. tostring(value))
+        value = type(value) == "function" and string.dump(value) or value
+        ConfigFile:write(index .. ":" .. tostring(value))
 
-    if _ ~= #self.indexes then
-      ConfigFile:write("\n")
+        if _ ~= #self.indexes then
+            ConfigFile:write("\n")
+        end
+
     end
 
-  end
-
-  ConfigFile:close()
+    ConfigFile:close()
 end
 
 return Config
