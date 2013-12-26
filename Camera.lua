fff4139f8672b57814c56fe54e742795d13375c8
unek <unekpl@gmail.com>
2013-12-26 19:36:30 +0100

    misc minor changes to camera

--- Camera.lua
+++ Camera.lua
@@ -1,4 +1,5 @@
 local Camera = Class("Camera")
+local min, max = math.min, math.max
 function Camera:initialize(x, y)
     self.pos   = {}
     self.pos.x = x or 0
@@ -14,11 +15,17 @@ function Camera:follow(entity)
     self.target = entity
 end
 function Camera:push()
-    local w, h = love.graphics.getWidth(), love.graphics.getHeight()
+    local w, h   = love.graphics.getDimensions()
+    local ww, wh = self.target.world:getSize()
 
     if self.target then
-        self.pos.x = math.min(math.max(self.target.pos.x, w / 2), game.world.width - w / 2)
-        self.pos.y = math.min(math.max(self.target.pos.y, h / 2), game.world.height - h / 2)
+        local x, y = self.target:getPosition()
+        if ww > w and wh > h then
+            self.pos.x = min(max(x, w / 2), ww - w / 2)
+            self.pos.y = min(max(y, h / 2), wh - h / 2)
+        else
+            self.pos.x, self.pos.y = x, y
+        end
     end
 
     love.graphics.push()
@@ -33,21 +40,21 @@ function Camera:getMousePosition()
     return self:toWorld(love.mouse.getPosition())
 end
 function Camera:toCamera(x, y)
-    local w, h = love.graphics.getWidth(), love.graphics.getHeight()
+    local w, h = love.graphics.getDimensions()
     local x = x - self.pos.x + w / 2
     local y = y - self.pos.y + h / 2
 
     return x, y
 end
 function Camera:toWorld(x, y)
-    local w, h = love.graphics.getWidth(), love.graphics.getHeight()
+    local w, h = love.graphics.getDimensions()
     local x = x - w / 2
     local y = y - h / 2
 
     return x + self.pos.x, y + self.pos.y
 end
 function Camera:getBounds()
-    local w, h = love.graphics.getWidth(), love.graphics.getHeight()
+    local w, h = love.graphics.getDimensions()
     local x, y = self.pos.x, self.pos.y
     return x - w / 2, y - h / 2 , x + w / 2, y + h / 2
 end
