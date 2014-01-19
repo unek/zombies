53f9c6af12437418d78692f39d8e7c8f180bf88d
unek <unekpl@gmail.com>
2014-01-19 01:02:54 +0100

    simplified the intro a lot

--- IntroState.lua
+++ IntroState.lua
@@ -2,15 +2,15 @@ intro = {}
 
 local timer = require("libraries.timer")
 local font  = love.graphics.newFont(90)
-local color = {0, 0, 0, 0}
-local vignette = love.graphics.newImage("assets/vignette.png")
+local color = {255, 255, 255, 0}
+local logo  = love.graphics.newImage("assets/love.png")
 
 function intro:init()
-    love.graphics.setBackgroundColor(255, 255, 255)
+    love.graphics.setBackgroundColor(0, 0, 0)
 
-    timer.tween(2, color, {0, 0, 0, 255}, "linear", function()
-        timer.add(2, function()
-            timer.tween(2, color, {0, 0, 0, 0}, "linear", function()
+    timer.tween(1, color, {255, 255, 255, 255}, "linear", function()
+        timer.add(1, function()
+            timer.tween(1, color, {255, 255, 255, 0}, "linear", function()
                 Gamestate.switch(game)
             end)
         end)
@@ -22,16 +22,11 @@ function intro:update(dt)
 end
 
 function intro:draw()
-    local label = "Zombies"
     local w, h  = love.graphics.getDimensions()
-    local x, y  = (w - font:getWidth(label)) / 2, (h - font:getHeight()) / 2
+    local x, y  = (w - logo:getWidth()) / 2, (h - logo:getHeight()) / 2
 
     love.graphics.setColor(color)
-    love.graphics.setFont(font)
-    love.graphics.print(label, x, y)
-
-    love.graphics.setColor(255, 255, 255, 180)
-    love.graphics.draw(vignette, 0, 0, 0, w / vignette:getWidth(), h / vignette:getHeight())
+    love.graphics.draw(logo, x, y)
 end
 
 function intro:keypressed(key, is_repeat)
