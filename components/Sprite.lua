9aab846296ba90bdea1fe3f476d0a7d100ddd014
unek <unekpl@gmail.com>
2014-01-19 12:55:08 +0100

    added Sprite:computeAABB thanks to Zer0

--- components/Sprite.lua
+++ components/Sprite.lua
@@ -63,3 +63,33 @@ function Sprite:testPoint(x, y)
     return leftX <= rotatedX and rotatedX <= rightX and
         topY <= rotatedY and rotatedY <= bottomY
 end
+
+-- thanks Zer0 for these methods!
+function Sprite:computeCorners()
+    local x, y = self.pos.x, self.pos.y
+    local w, h = self.width * self.scale.x, self.height * self.scale.y
+    local r    = self.rotation
+
+    local r1 = math.atan2(h, w)
+    local r2 = math.atan2(-h, w)
+    local r3 = math.atan2(-h, -w)
+    local r4 = math.atan2(h, -w)
+
+    local d = math.sqrt((h / 2) ^ 2 + (w / 2) ^ 2)
+
+    local x1, y1 = x + math.cos(r1 + r) * d, y + math.sin(r1 + r) * d
+    local x2, y2 = x + math.cos(r2 + r) * d, y + math.sin(r2 + r) * d
+    local x3, y3 = x + math.cos(r3 + r) * d, y + math.sin(r3 + r) * d
+    local x4, y4 = x + math.cos(r4 + r) * d, y + math.sin(r4 + r) * d
+
+    return x1, y1, x2, y2, x3, y3, x4, y4
+end
+
+function Sprite:computeAABB()
+    local x1, y1, x2, y2, x3, y3, x4, y4 = self:computeCorners()
+
+    local bx, by = math.min(x1, x2, x3, x4), math.min(y1, y2, y3, y4)
+    local tx, ty = math.max(x1, x2, x3, x4), math.max(y1, y2, y3, y4)
+
+    return bx, by, tx - bx, ty - by
+end
